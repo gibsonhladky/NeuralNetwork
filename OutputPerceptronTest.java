8c58b5296fdd15a3e0c0c3237b046b0e5d135385
gibsonhladky <hladky@wisc.edu>
2016-06-03 14:40:56 -0500

    Added OutputPerceptron tests

--- OutputPerceptronTest.java
+++ OutputPerceptronTest.java
@@ -2,11 +2,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import neuralNetwork.OutputPerceptron;
+
 public class OutputPerceptronTest {
 
+	final double DELTA = 0.001;
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void negativeInputSize()
+	{
+		OutputPerceptron testP = new OutputPerceptron(-1);
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void correctOutput() {
+		OutputPerceptron testP = new OutputPerceptron(2);
+		double[] testInputs = {0.5, 0.5};
+		double[] testWeights = {1.0, -1.0};
+		
+		testP.setInputs(testInputs);
+		testP.setWeights(testWeights);
+		
+		testP.activate();
+		
+		final double expectedOutput = 0.5;
+		assertEquals(expectedOutput, testP.output(), DELTA);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void setInputsTooSmall()
+	{
+		OutputPerceptron testP = new OutputPerceptron(2);
+		double[] testInputs = {1.0};
+		testP.setInputs(testInputs);
 	}
 
 }
--- neuralNetwork/OutputPerceptron.java
+++ neuralNetwork/OutputPerceptron.java
@@ -2,17 +2,20 @@ package neuralNetwork;
 
 public class OutputPerceptron implements Perceptron {
 
+	int inputSize;
 	
+	double[] inputs;
+	double[] weights;
 	
-	public OutputPerceptron() {
-		
-		// TODO Auto-generated constructor stub
+	double output;
+	
+	public OutputPerceptron(int inputSize) {
+		this.inputSize = inputSize;
 	}
 
 	@Override
 	public void activate() {
-		// TODO Auto-generated method stub
-		
+		output = Perceptron.activationFunction(inputValue());
 	}
 
 	@Override
@@ -29,8 +32,35 @@ public class OutputPerceptron implements Perceptron {
 
 	@Override
 	public double output() {
-		// TODO Auto-generated method stub
-		return 0;
+		return output;
+	}
+	
+	public void setInputs(double[] newInputs)
+	{
+		if(newInputs.length != inputSize)
+		{
+			throw new IllegalArgumentException("Inputs must match input size of the perceptron.");
+		}
+		this.inputs = newInputs;
+	}
+	
+	public void setWeights(double[] newWeights)
+	{
+		if(newWeights.length != inputSize)
+		{
+			throw new IllegalArgumentException("Inputs must match input size of the perceptron.");
+		}
+		weights = newWeights;
+	}
+	
+	private double inputValue()
+	{
+		double sumOfWeightedInputs = 0;
+		for(int i = 0; i < inputSize; i++)
+		{
+			sumOfWeightedInputs += inputs[i] * weights[i];
+		}
+		return sumOfWeightedInputs;
 	}
 
 }
