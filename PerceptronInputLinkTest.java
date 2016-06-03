cbdb9d8bd46ab825bd10546ea6ab0d26658023e9
gibsonhladky <hladky@wisc.edu>
2016-06-03 18:28:34 -0500

    Created MockWeightGenerator
    MockWeightGenerator is used in testing to always produce the value 1 as
    a weight, to enable simple testing.

--- PerceptronInputLinkTest.java
+++ PerceptronInputLinkTest.java
@@ -5,23 +5,34 @@ import org.junit.Test;
 import neuralNetwork.PerceptronInputLink;
 
 public class PerceptronInputLinkTest {
+	
+	final double DELTA = 0.001;
 
 	@Test
 	public void constructorNoArgsCreatesOnlyRandomBias()
 	{
 		PerceptronInputLink testLink = new PerceptronInputLink();
-		double[] actualInputs = testLink.inputValues();
+		final double[] actualInputs = testLink.inputValues();
 		
-		int expectedLength = 1;
+		final int expectedLength = 1;
 		assertEquals(expectedLength, actualInputs.length);
-		double biasValue = actualInputs[0];
+		
+		final double biasValue = actualInputs[0];
 		assertTrue(biasValue >= -1 && biasValue <= 1.0);
 	}
 	
 	@Test
 	public void constructorOneArgsCreatesOnlyBiasFromWeightGen()
 	{
+		PerceptronInputLink testLink = new PerceptronInputLink(new MockWeightGenerator());
+		final double[] actualInputs = testLink.inputValues();
 		
+		final int expectedLength = 1;
+		assertEquals(expectedLength, actualInputs.length);
+		
+		final double actualBiasValue = actualInputs[0];
+		final double expectedBiasValue = 1;
+		assertEquals(expectedBiasValue, actualBiasValue, DELTA);
 	}
 
 }
