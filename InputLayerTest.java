fb4379fa0cad26fc9e5cb310aee34e195a83d83e
gibsonhladky <hladky@wisc.edu>
2016-06-03 12:43:33 -0500

    Cleared Yoda values

--- InputLayerTest.java
+++ InputLayerTest.java
@@ -5,12 +5,14 @@ import org.junit.Test;
 import neuralNetwork.InputLayer;
 
 public class InputLayerTest {
+	
+	final double DUMBYINPUT = 1.0;
 
 	@Test (expected = IllegalArgumentException.class)
 	public void setInputsTooSmall()
 	{
 		InputLayer testLayer = new InputLayer(2);
-		double[] testInputs = {1.0};
+		double[] testInputs = {DUMBYINPUT};
 		testLayer.setInputs(testInputs);
 	}
 	
@@ -18,14 +20,16 @@ public class InputLayerTest {
 	public void setInputsTooLarge()
 	{
 		InputLayer testLayer = new InputLayer(2);
-		double[] testInputs = {1.0, 1.0, 1.0};
+		double[] testInputs = {DUMBYINPUT, DUMBYINPUT, DUMBYINPUT};
 		testLayer.setInputs(testInputs);
 	}
 	
 	@Test
 	public void feedForwardActivationCalculation()
 	{
-		
+		InputLayer testLayer = new InputLayer(2);
+		double[] testInputs = {DUMBYINPUT, DUMBYINPUT};
+		testLayer.setInputs(testInputs);
 	}
 
 }
