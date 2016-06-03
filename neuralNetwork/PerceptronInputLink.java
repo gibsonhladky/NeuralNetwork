fd9d5f659ab99dcf0ed8d41776b4bfde68acd515
gibsonhladky <hladky@wisc.edu>
2016-06-03 17:42:08 -0500

    Created BiasPerceptron
    Bias Perceptron serves as a dummy perceptron only used in links to
    account for the bias calculation in perceptron activations.

--- neuralNetwork/PerceptronInputLink.java
+++ neuralNetwork/PerceptronInputLink.java
@@ -1,7 +1,23 @@
 package neuralNetwork;
 
+import java.util.ArrayList;
+
+/*
+ * Perceptron input links store the (output, weight)
+ * pairs for connections between perceptrons.
+ * The link provides weighted input values for
+ * the perceptron, and handles the management of 
+ * which perceptrons are connected to it.
+ */
 public class PerceptronInputLink {
 
+	private ArrayList<Perceptron> inputPerceptrons;
+	private ArrayList<Double> inputWeights;
 	
+	public PerceptronInputLink()
+	{
+		inputPerceptrons = new ArrayList<Perceptron>();
+		inputPerceptrons.add(new BiasPerceptron());
+	}
 	
 }
