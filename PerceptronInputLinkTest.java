9b8acba4c9571ce36ac217bf17fecb3526cf7e72
gibsonhladky <hladky@wisc.edu>
2016-06-05 14:08:08 -0500

    Implemented constructor test for input links
    Implemented the two argument constructor for input links. Added
    MockPerceptron to more easily test units using perceptrons.

--- PerceptronInputLinkTest.java
+++ PerceptronInputLinkTest.java
@@ -1,7 +1,11 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
+import neuralNetwork.InputPerceptron;
+import neuralNetwork.Perceptron;
 import neuralNetwork.PerceptronInputLink;
 
 public class PerceptronInputLinkTest {
@@ -22,7 +26,7 @@ public class PerceptronInputLinkTest {
 	}
 	
 	@Test
-	public void constructorOneArgsCreatesOnlyBiasFromWeightGen()
+	public void constructorOneArgsCreatesOnlyBiasFromWeightGenerator()
 	{
 		PerceptronInputLink testLink = new PerceptronInputLink(new MockWeightGenerator());
 		final double[] actualInputs = testLink.inputValues();
@@ -33,6 +37,24 @@ public class PerceptronInputLinkTest {
 		final double actualBiasValue = actualInputs[0];
 		final double expectedBiasValue = 1;
 		assertEquals(expectedBiasValue, actualBiasValue, DELTA);
+	}
+	
+	@Test
+	public void constructorTwoArgsCreatesBiasAndAllWeightsFromWeightGenerator()
+	{
+		ArrayList<Perceptron> inputs = new ArrayList<Perceptron>(2);
+		inputs.add(new MockPerceptron());
+		inputs.add(new MockPerceptron());
+		
+		PerceptronInputLink testLink = new PerceptronInputLink(new MockWeightGenerator(), inputs);
+		
+		final double[] actualInputs = testLink.inputValues();
+		
+		final int expectedLength = 3;
+		assertEquals(expectedLength, actualInputs.length);
+		
+		final double[] expectedInputValues = {1.0, 0.5, 0.5};
+		assertArrayEquals(expectedInputValues, actualInputs, DELTA);
 	}
 
 }
