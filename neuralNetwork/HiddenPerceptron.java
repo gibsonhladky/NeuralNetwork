c4a4be8a317e7d6600d90d33bfc1d42712c30016
gibsonhladky <hladky@wisc.edu>
2016-06-07 14:57:21 -0500

    Removed unused parts of HiddenPerceptron
    After introducing input and output links, the previous implementations
    for hidden perceptron methods were replaced.

--- neuralNetwork/HiddenPerceptron.java
+++ neuralNetwork/HiddenPerceptron.java
@@ -1,19 +1,10 @@
 package neuralNetwork;
-import java.util.ArrayList;
-import java.util.Random;
 
 public class HiddenPerceptron implements Perceptron
 {	
 	public static final double LEARNING_RATE = 0.1;
 	
-	public int index; // current perceptron's index with it's layer
-	public ArrayList<HiddenPerceptron> outputs; // next layer of perceptrons
-	public ArrayList<HiddenPerceptron> inputs; // previous layer of perceptrons
-	
-	public boolean inputLayer;
 	public double output;
-	public ArrayList<Double> inputWeights;
-	public double bias;
 	
 	public double error;
 	
@@ -23,30 +14,6 @@ public class HiddenPerceptron implements Perceptron
 	
 	public HiddenPerceptron(WeightGenerator wg)
 	{
-		if(index < 0)
-		{
-			throw new IllegalArgumentException("Negative Perceptron index: " + index);
-		}
-		bias = wg.nextWeight();
-		
-		if(inputs != null)
-		{
-			int size = inputs.size();
-			inputWeights = new ArrayList<Double>(size);
-			for(int i = 0; i < size; i++)
-			{
-				double weight = wg.nextWeight();
-				inputWeights.add(weight);
-			}
-		}
-		else
-		{
-			inputLayer = true;
-			inputWeights = new ArrayList<Double>();
-		}
-		this.outputs = null;
-		
-		
 		inputLinks = new InputLinks(wg);
 		outputLinks = new OutputLink(wg);
 	}
@@ -67,14 +34,7 @@ public class HiddenPerceptron implements Perceptron
 	@Override
 	public void adjustToError()
 	{
-		
-		bias = bias - LEARNING_RATE * error * output;
-		
-		for(int i = 0; i < inputWeights.size(); i++)
-		{
-			double weight = inputWeights.get(i);
-			inputWeights.set(i, weight - LEARNING_RATE * error * output);
-		}
+		error = outputLinks.getAssociatedError();
 	}
 
 	@Override
