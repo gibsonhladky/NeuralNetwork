82a98ba4a9e7b881f343435db585720b6332c53c
gibsonhladky <hladky@wisc.edu>
2016-06-07 13:34:50 -0500

    Implemented Tests
    Added several tests verifying the state-correctness of Perceptron
    interface methods in HiddenPerceptron.

--- HiddenPerceptronTest.java
+++ HiddenPerceptronTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 
 import java.util.ArrayList;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import neuralNetwork.HiddenPerceptron;
@@ -11,22 +12,31 @@ public class HiddenPerceptronTest {
 
 	private final double DELTA = 0.001;
 	
-	@Test
-	public void outputDoesNotChangeState()
+	HiddenPerceptron testP;
+	
+	@Before
+	public void setup()
 	{
-		HiddenPerceptron testP = new HiddenPerceptron(new MockWeightGenerator());
+		testP = new HiddenPerceptron(new MockWeightGenerator());
+		testP.addInputLink(new MockPerceptron());
+		testP.addInputLink(new MockPerceptron());
+		testP.addOutputLink(new MockPerceptron());
+		testP.addOutputLink(new MockPerceptron());
+	}
+	
+	@Test
+	public void outputAndErrorDoNotChangeState()
+	{
 		final double expectedOutput = testP.output();
+		final double expectedError = testP.error();
 		
 		assertEquals(expectedOutput, testP.output(), DELTA);
+		assertEquals(expectedError, testP.error(), DELTA);
 	}
 	
 	@Test
 	public void activateCorrectlyChangesOutput()
-	{
-		HiddenPerceptron testP = new HiddenPerceptron(new MockWeightGenerator());
-		testP.addInputLink(new MockPerceptron());
-		testP.addInputLink(new MockPerceptron());
-		
+	{	
 		testP.activate();
 		
 		final double expectedOutput = 0.8807971;
@@ -34,9 +44,39 @@ public class HiddenPerceptronTest {
 	}
 	
 	@Test
-	public void activateChangesOutput()
+	public void activateChangesOutputOnNewInputs()
 	{
+		final double priorOutput = testP.output();
+		testP.activate();
 		
+		assertNotEquals(priorOutput, testP.output(), DELTA);
+	}
+	
+	@Test
+	public void activateDoesNotChangeError()
+	{
+		final double priorOutput = testP.error();
+		testP.activate();
+		
+		assertEquals(priorOutput, testP.error(), DELTA);
+	}
+	
+	@Test
+	public void adjustToErrorDoesNotChangeOutput()
+	{
+		final double priorOutput = testP.output();
+		testP.adjustToError();
+		
+		assertEquals(priorOutput, testP.output(), DELTA);
+	}
+	
+	@Test
+	public void adjustToErrorCorrectlyChangesError()
+	{
+		testP.activate();
+		
+		final double expectedOutput = 0.8807971;
+		assertEquals(expectedOutput, testP.output(), DELTA);
 	}
 	
 }
