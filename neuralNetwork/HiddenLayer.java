c85ea577e7bdad0d79f07e1e2ba54053de348ec3
gibsonhladky <hladky@wisc.edu>
2016-06-08 11:49:14 -0500

    Implemented and tested parts of HiddenLayer

--- neuralNetwork/HiddenLayer.java
+++ neuralNetwork/HiddenLayer.java
@@ -1,7 +1,23 @@
 package neuralNetwork;
 
+import java.util.List;
+
 public class HiddenLayer implements NetworkLayer {
 
+	private List<NetworkLayer> previousLayers;
+	
+	public HiddenLayer(int size, WeightGenerator wg)
+	{
+		if(size < 1)
+		{
+			throw new IllegalArgumentException("Cannot create a hidden layer with zero or negative size.");
+		}
+		if(wg == null)
+		{
+			throw new IllegalArgumentException("Cannot create a hidden layer with null weight generator.");
+		}
+	}
+	
 	@Override
 	public void activate() {
 		// TODO Auto-generated method stub
@@ -24,6 +40,20 @@ public class HiddenLayer implements NetworkLayer {
 	public Perceptron[] perceptrons() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void addPreviousLayer(NetworkLayer prev)
+	{
+		if(prev == null)
+		{
+			throw new IllegalArgumentException("Cannot add a null previous network layer to a hidden layer.");
+		}
+		previousLayers.add(prev);
+	}
+	
+	public List<NetworkLayer> prevousLayers()
+	{
+		return previousLayers;
 	}
 
 }
